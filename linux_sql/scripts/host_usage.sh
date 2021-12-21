d8a3c5da1d1b8839c29f8f17e8175dd313bb83ff
Nick Lesho <nick.lesho@gmail.com>
2021-12-21 02:01:23 +0000

    Fixed issue with host_usage.sh

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -18,11 +18,11 @@ memory_free=$(echo "$vmstat_mb" | awk '{print $4}'| tail -n1 | xargs)
 cpu_idle=$(echo "$vmstat_mb" | awk '{print $15}'| tail -n1 | xargs)
 cpu_kernel=$(echo "$vmstat_mb" | awk '{print $14}'| tail -n1 | xargs)
 disk_io=$(vmstat -d | awk '{print $9}' | tail -n1 | xargs)
-disk_available=$(df -BM / | awk '{print $4}'| tail -n1 | xargs)
+disk_available=$(df -BM / | awk '{print $4}'| tail -n1 | cut -c -5 | xargs)
 
 timestamp=$(vmstat -t | awk '{print $18" "$19}'| tail -n1 | xargs)
 
-host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
+host_id=1;
 
 insert_stmt="INSERT INTO host_usage(timestamp, host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available) VALUES('$timestamp', '$host_id', '$memory_free', '$cpu_idle', '$cpu_kernel', '$disk_io', '$disk_available')";
 
