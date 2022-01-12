f5ca5446b77bcba3a417ffcb21f2b0a4890bcdcb
Raz0rSharp <nick.lesho@gmail.com>
2022-01-12 10:09:20 -0500

    Created grep interface file

---
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrep.java
@@ -0,0 +1,50 @@
+public interface JavaGrep {
+
+    /**
+     * Top level search workflow
+     * @throws IOException
+     */
+    void process() throws IOException;
+
+    /**
+     * Traverse a given directory and return all files
+     * @param rootDir
+     * @return
+     */
+    List<file> listFiles(String rootDir);
+
+    /**
+     * Read a file and return all the lines.
+     *
+     * @param inputFile
+     * @return
+     */
+    List<String> readLines(File inputFile);
+
+    /**
+     * check if a line contains the regex pattern (passed by user)
+     * @param line
+     * @return
+     */
+    boolean containsPattern(String line);
+
+    /**
+     * Write lines to a file
+     * @param lines
+     * @throws IOException
+     */
+    void writeToFile(List<String> lines) throws IOException;
+
+    String getRootPath();
+
+    void setRootPath(String rootPath);
+
+    String getRegex();
+
+    void setRegex(String regex);
+
+    String getOutFile();
+
+    void setOutFile(String outFile);
+}
+
